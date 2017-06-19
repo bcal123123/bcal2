8a5698ac918a6706011068d114b8a2f558a7fd91
a@a.com <a@a.com>
2017-06-19 15:28:27 +0200

    dasd

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var port = process.env.port || 8200;
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(80);
+app.listen(8200);
